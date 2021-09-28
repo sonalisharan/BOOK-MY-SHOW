5cc1a86baa5d088bb3c100fb8d373d402d745990
sonalisharan <angelsona1627@gmail.com>
2021-09-28 09:26:32 +0530

    HOC and Layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
-  return <div className="App">
-    <h1 className="text-6xl">
-          Hello People, welcome to tailwind
-    </h1>
-  </div>;
+import { Route } from "react-router-dom";
+
+function Name() {
+  return <h1> Hello Root </h1>;
 }
+
+function Name2() {
+  return <h1>Hello movie</h1>;
+}
+
+function App() {
+  return (
+    <>
+    <Route path="/" exact component ={Name} />
+    <Route path="/movie" exact component ={Name2} />
+    </>
+  );
+};
 
 export default App;
---
+++ src/HOC/Default.HOC.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Route} from "react-router-dom";
+
+//Layout 
+import DefaultLayout from "../layouts/Default.layout";
+
+const DefaultHOC = () => {
+    return <>
+
+    </>;
+};
+
+
+export default DefaultHOC;
