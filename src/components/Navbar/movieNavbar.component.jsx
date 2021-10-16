5eb0278bcd2b8a3999061c3ab8e8b5314e22b306
sonalisharan <angelsona1627@gmail.com>
2021-10-16 22:14:34 +0530

    src components

--- src/components/Navbar/movieNavbar.component.jsx
+++ src/components/Navbar/movieNavbar.component.jsx
@@ -1,40 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
   BiChevronRight,
   BiSearch,
   BiMenu,
   BiChevronDown,
+  BiShareAlt,
 } from "react-icons/bi";
 
+// Context
+import { MovieContext } from "../../context/movie.context";
+
 const NavSm = () => {
+  const { movie } = useContext(MovieContext);
   return (
     <>
       <div className="text-white flex items-center justify-between">
         <div>
-          <h3 className="text-xl font-bold">It All starts Here!</h3>
-          <span className="text-gray-400 text-xs flex items-center">
-            Bengaluru <BiChevronRight />
-          </span>
+          <h3 className="text-xl font-bold">{movie.original_title}</h3>
         </div>
         <div className="w-8 h-8">
-          <BiSearch className="w-full h-full" />
+          <BiShareAlt className="w-full h-full" />
         </div>
       </div>
     </>
   );
 };
-const NavMd = () => {
-  return (
-    <div className="w-full flex items-center gap-3 bg-white px-3 py-2 rounded-md">
-      <BiSearch />
-      <input
-        type="search"
-        className="w-full bg-transparent border-none focus:outline-none"
-        placeholder="Search for movies, events, Plays, Sports and Activities."
-      />
-    </div>
-  );
-};
+
 const NavLg = () => {
   return (
     <>
@@ -72,17 +63,17 @@ const NavLg = () => {
   );
 };
 
-const Navbar = () => {
+const MovieNavbar = () => {
   return (
     <>
-      <nav className="bg-bms-700 p-4">
+      <nav className="absolute inset-x-0 z-30 bg-opacity-10 backdrop-filter backdrop-blur-lg lg:relative lg:bg-bms-700 p-4">
         <div className="md:hidden">
           {/* Mobile screen */}
           <NavSm />
         </div>
-        <div className="hidden md:flex lg:hidden">
+        <div className="hidden md:block lg:hidden">
           {/* Medium/Tab screen */}
-          <NavMd />
+          <NavSm />
         </div>
         <div className="hidden w-full lg:flex">
           {/* Large screen */}
@@ -93,4 +84,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default MovieNavbar;
